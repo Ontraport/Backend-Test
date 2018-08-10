b0c7f360f3f7670836ff51bf16f11bc03c27432e
iinno <iinno@github.com>
2018-08-09 23:29:02 -0700

    Everything works, gotta clean it up.

--- Q2.rb
+++ Q2.rb
@@ -2,28 +2,49 @@ def decomp_helper(partMap)
 	temp = partMap.keys[0].split("/", 2)[0]
 	tempMap = Hash.new
 	finalMap = Hash.new
+	temparray = [];
 	partMap.each do |key, value|
+
 		toks = key.split("/", 2)
+
 		if toks[0] == temp
+
+			isArray = Integer(toks[1]) rescue false
+
 			if toks[1].instance_of?(NilClass)
 				finalMap.store(toks[0], value)
+			elsif isArray
+				temparray << value
+				puts temparray
 			else
 				tempMap.store(toks[1], value)
 			end
+
 		else 
-			if not tempMap.empty?
+
+			if not temparray.empty?
+				finalMap.store(temp, temparray)
+				temparray.clear
+			elsif not tempMap.empty?
 				finalValue = decomp_helper(tempMap)
 				finalMap.store(temp, finalValue)
 				tempMap.clear
 			end
+			
 			temp = toks[0]
 			redo
 		end
 	end
-	if not tempMap.empty?
+
+
+	if not temparray.empty?
+			finalMap.store(temp, temparray)
+			temparray = Array.new
+	elsif not tempMap.empty?
 		finalValue = decomp_helper(tempMap)
 		finalMap.store(temp, finalValue)
 	end
+
 	return finalMap
 end
 
@@ -52,7 +73,9 @@ end
 
 example = {
 	"one/two" => 3,
-	"one/four" => 5,
+	"one/four/0" => 5,
+	"one/four/1" => 6,
+	"one/four/2" => 7,
 	"eight/nine/ten" => 11
     }
 
