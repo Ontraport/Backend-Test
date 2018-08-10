c665956387edc83c333197c1926bf11cf7af5c83
iinno <iinno@github.com>
2018-08-10 01:50:18 -0700

    Finished.

--- Q1.rb
+++ Q1.rb
@@ -1,18 +1,48 @@
+#!/usr/bin/env ruby
+
+##
+# Question 1: Write a function that accepts a multi-dimensional container of any 
+# size and converts it into a one dimensional associative array whose keys are strings 
+# representing their value's path in the original container.
+
+# This helper function takes the multi-dimensional container and iterates through each 
+# element and builds the final one-dimensional associative array in a recursive down-top order.
+#   1. For each element in the container check the values to see 
+#        if it's another container, array or a number.
+#   2. If map, recusively call the helper function on that map object with updated path 
+#          and current associative array (oneD).
+#   3. Else, store key and the value to current associative array (oneD).
+#
+#     
+# params
+# => "multiD" is the multi dimensional container that is passed.
+# => "path" is the parameter to remember and create the current path.
+# => "oneD" is the one-dimensional array that is updated and populated (since Ruby is pass by value)
+#           it needs to be updated and reassigned.
+
+
+
 def compile_helper(multiD, path, oneD)
+    # Check if the helper function has called for the first time.
     if not path.eql?("")
         path = path + "/"
     end
 
     multiD.each do |key, value|
 
+        # If the value is a nother container, recursively call the helper function and pass the value, 
+        # the current path as well as the current key and finally the associative array.
         if value.kind_of?(Hash)
             oneD = compile_helper(value, path + key.to_s, oneD)
+
+        # If the value is an array, create a new path for each element. Use index as the path.
         elsif value.kind_of?(Array) then
             i = 0
             value.each do |element|
                 oneD.store(path + key.to_s + "/" + i.to_s, element)
                 i += 1
             end
+        # Else, store the path as the key and the number as the value.
         else 
             oneD.store(path + key.to_s, value)
         end
@@ -21,6 +51,10 @@ def compile_helper(multiD, path, oneD)
 
     return oneD
 end
+
+#Main function, calls the helper function also creates a initial empty map.
+#Param
+# => "nstedMap" is the multi-dimensional container of any size.\
 
 def compile_multi(nstedMap)
     oneDArray = Hash.new
--- Q2.rb
+++ Q2.rb
@@ -1,9 +1,33 @@
-def decompiler_helper(partMap)
+#!/usr/bin/env ruby
+
+##
+# Question 2: Convert a one dimensional associative array created from question 1
+# back to a multi-dimensional container of any size.
+
+
+# This helper function takes the elements of the array and creates containers according to the path
+# in a recursive top-down order. 
+# 	1. It parses the parent and the rest(child) of the path
+# 	2. Checks for various conditions such as is rest of the path nil? and is the path part of a array?
+# 	3. Push the appropriate values to the variable finalMap (maps, values, arrays).
+# 	4. Finally, iterate through finalMap to see if the value is a instance of a map, 
+#	   recursively call on that map object.
+#
+# 	
+# param
+# => "partMap" is initally the complete one dimensional associative array, 
+#    then partial maps where a paths still exist.
+
+def decompile_helper(partMap)
 	finalMap = Hash.new
+
 	partMap.each do |key, value|
 		parent, child = key.split("/", 2)
+
+		#Check if the child is part of a array.
 		isArray = Integer(child) rescue false
 
+		#Cases to check for before populating finalMap.
 		if child.instance_of?(NilClass)
 			finalMap[parent] = value
 		elsif isArray && finalMap.key?(parent)
@@ -18,12 +42,18 @@ def decompiler_helper(partMap)
 	end
 
 	finalMap.each do |key, value|
+		#Recursively call if the path still exists (i.e. value is a map).
 		if value.instance_of?(Hash)
 			finalMap[key] = decompile_helper(value)
 		end
 	end
+
 	return finalMap
 end
+
+# Main function, calls the helper function.
+# param
+# => "oneDArray" is the one dimensional associative array to be converted.
 
 def decompile_single(oneDArray)
 	puts decompile_helper(oneDArray)
